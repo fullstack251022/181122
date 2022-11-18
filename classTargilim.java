0eb4e11322a61f8472dbefc60e0c96653088cf19
fullstack251022 <116830126+fullstack251022@users.noreply.github.com>
2022-11-18 12:58:33 +0200

    Create classTargilim.java

---
+++ classTargilim.java
@@ -0,0 +1,87 @@
+//public class Main {
+//    public static void main(String[] args) {
+//        //targil 1:
+//        //1)create array of integers. Size of 7
+//        //2) print first number, last number and middle number
+//        //3) print first number, last number and middle number as if you don't know initial size of the array
+//
+////        String x = "mystring";
+////        Math.max(2,5);
+//
+////        //   //targil 1
+////        int[] myIntegersArray = {9, 3, 7, 9, 5, 8, 10, 12, 13};
+////        System.out.println(myIntegersArray[0]); //print first number //will work always
+////        System.out.println(myIntegersArray[myIntegersArray.length-1]); //always work
+////
+////        int middleIndex = (myIntegersArray.length-1)/2;
+////        System.out.println(myIntegersArray[middleIndex]);
+////        System.out.println(myIntegersArray[(myIntegersArray.length-1)/2]); //always work
+//
+//
+//        //targil 2
+//        //create array of integers. Size of 5
+//        //print array numbers in reverse order (use loop)
+//        //dugma: array:[5,8,10,13,2]   sout > 2,13,10,8,5
+//
+//
+//        int[] myIntArr = {2, 6, 2, 8, 5, 6, 10};
+//
+////        for (int index = 0; index < myIntArr.length; index++) {
+////            System.out.println(myIntArr[index]);
+////        }
+//
+//        //dereh 1
+////        for (int index = myIntArr.length-1; index >=0 ; index--) {
+////            System.out.println(myIntArr[index]);
+////        }
+//
+////        for (int i = 0; i < myIntArr.length; i++) { //I don't like this
+////            System.out.println("i: "+i);
+////            System.out.println("index: "+((myIntArr.length-1)-i));
+//////            System.out.println(myIntArr[(myIntArr.length-1)-i]);
+////        }
+//
+//
+//        // 2 , 3 , 5 , 5 , 3 , 2     //palindrome
+//        // 2 , 3 , 5 , 5 , 8 , 2     //not palindrome
+//
+////        int[] myPalindrome = {2, 4, 1, 5, 4, 2};
+////        boolean isPalindrome = true;
+////        for (int i = 0, j=myPalindrome.length-1 ; i < myPalindrome.length; i++ , j--) {
+////            if(myPalindrome[i]!=myPalindrome[j]){
+////                isPalindrome = false;
+////                break;
+////            }
+////        }
+////
+////        if(isPalindrome){
+////            System.out.println("Palindrome");
+////        }else{
+////            System.out.println("Not Palindrome");
+////        }
+//
+////        int[] arrayGadol = new int[1000];//length 1000
+//
+////        //1) create an array A of integer numbers with these numbers: 4, 10, 20, 40, 1000
+////        int[] arrayA = {13, 5, 20, 3, 8, 12 ,10 ,5 };//length 1000
+////
+////        //2        create a second array B with these numbers: 0, 0, 0, 0, 0
+////        int[] arrayB = {0, 0, 0, 0, 0, 0 ,0, 0};
+//////        arrayB[0] = arrayA[0];
+//////        arrayB[1] = arrayA[1];
+//////        arrayB[2] = arrayA[2];
+//////        arrayB[3] = arrayA[3];
+//////        arrayB[4] = arrayA[4];
+////
+//////        System.out.println(arrayB[0]);
+////
+////        for (int i = 0; i < arrayA.length; i++) {
+////            arrayB[i]=arrayA[i];
+////        }
+////
+////
+////        for (int i = 0; i <arrayB.length ; i++) {
+////            System.out.print(arrayB[i]+" ");
+////        }
+////    }
+//}
