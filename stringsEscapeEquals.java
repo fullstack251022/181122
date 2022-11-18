97b6953f685004399d6cb08a7bef03fab4fa21a0
fullstack251022 <116830126+fullstack251022@users.noreply.github.com>
2022-11-18 12:57:59 +0200

    Create stringsEscapeEquals.java

---
+++ stringsEscapeEquals.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+//        String myString = "We are the so-called \\Vikings\\ from the north";
+//        System.out.println(myString);
+
+//        String myStringA = new String("test");
+//        String myStringB = new String("test");
+////        String myStringA = "test";
+////        String myStringB = "test";
+//
+//        if (myStringA.equals(myStringB)) {
+//            System.out.println();
+//        } else {
+//            System.out.println("False");
+//        }
+
+//        Math.floorDivExact()
+    }
+}
